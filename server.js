9a756eec781579f89a47033166f4b2efd96c213b
Kskalyani14 <kalyani.kondagunturi@gmail.com>
2025-04-14 14:58:59 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.end('Hello from Kalyani and Chitti!');
+});
+
+server.listen(8080, () => {
+  console.log('Server running on port 8080');
+});
